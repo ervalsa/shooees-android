a20e2c422b050e1c0153807079a9e4d933e6a8f0
ervalsa <ervalsa-san@gmail.com>
2022-10-10 22:02:03 +0800

    Implement binding to detail screen

--- app/src/main/java/com/sepatu/shooees/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/sepatu/shooees/ui/detail/DetailActivity.kt
@@ -3,10 +3,15 @@ package com.sepatu.shooees.ui.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sepatu.shooees.R
+import com.sepatu.shooees.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
