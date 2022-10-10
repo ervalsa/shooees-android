d3b849e0fcc3e4ec1bc00be4526ac8987cb209c9
ervalsa <ervalsa-san@gmail.com>
2022-10-10 22:04:10 +0800

    Add extras

--- app/src/main/java/com/sepatu/shooees/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/sepatu/shooees/ui/detail/DetailActivity.kt
@@ -3,9 +3,14 @@ package com.sepatu.shooees.ui.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sepatu.shooees.R
+import com.sepatu.shooees.data.entity.ProductEntity
 import com.sepatu.shooees.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val extra_data = "EXTRA_DATA"
+    }
 
     private lateinit var binding: ActivityDetailBinding
 
@@ -13,5 +18,10 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+    }
+
+    fun populate(productEntity: ProductEntity) {
+
     }
 }
